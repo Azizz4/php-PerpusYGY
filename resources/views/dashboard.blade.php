4ada9f2212c0ef8fc8f9ec96bdf292f8cc185534
quinn-java <muhamadsyahrilaziz@smkwikrama.sch.id>
2022-02-02 11:45:25 +0700

    YGY

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,13 @@
+@extends('layout.master')
+@section('content')
+<h1>Dashboard</h1>
+<!-- <img src="assets/admin/img/library.jpg" alt="library.jpg" width="500" height="500" >
+<img src="assets/admin/img/library.jpg" alt="library.jpg" width="500" height="500" float="left"> -->
+<div class="ygy">
+  <img src="assets/admin/img/notebook.jpg" alt="Notebook" style="width:69%">
+  <div class="ges">
+    <h1>Perpustakaan</h1>
+    <p>YaGesYaaaaaa</p>
+  </div>
+</div>
+@endsection
